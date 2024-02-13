c675ab9fdb69f10ebef7745457e6831059c3eafa
Fumiya Yamashita <4547751+tahmarrrr23@users.noreply.github.com>
2024-02-14 03:02:42 +0900

    update setup script

--- setup.sh
+++ setup.sh
@@ -1,43 +1,25 @@
 #!/bin/sh
 set -eu
 
-echo ""
-echo "##################"
-echo "#    Howdy!🤠    #"
-echo "##################"
-echo ""
-
-echo "🤠 rosetta 2"
 if [ ! -d "/Library/Apple/usr/share/rosetta" ]; then
   softwareupdate --install-rosetta --agree-to-license
 else
-  echo "already installed"
+  echo "rosetta 2 already installed"
 fi
 
-echo ""
-
-echo "🤠 homebrew"
 if [ ! -d "/opt/homebrew" ]; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
-  echo "already installed"
+  echo "homebrew already installed"
 fi
 
-echo ""
-
-echo "🤠 essential software"
 /opt/homebrew/bin/brew tap homebrew/cask-fonts
+
 /opt/homebrew/bin/brew install \
   google-chrome \
   google-japanese-ime \
   rectangle \
   scroll-reverser \
   logitech-g-hub \
-  font-hackgen \
-  font-hackgen-nerd
-
-echo ""
-echo "##################"
-echo "#    Adios!🤠    #"
-echo "##################"
-echo ""
+  font-plemol-jp \
+  font-plemol-jp-nf
