d452010e102a53a559dc2623559b81e009111649
Fumiya Yamashita <138208610+tahmarrrr23@users.noreply.github.com>
2024-12-20 23:33:23 +0900

    update scripts

---
+++ backup.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -eu
+cd "$(dirname "$0")"
+root_path=$PWD
+
+brew_cmd="/opt/homebrew"
+if [ -f "$brew_cmd" ]; then
+  eval "$($brew_cmd shellenv)"
+else
+  printf "Please install Homebrew\n"
+  exit 1
+fi
+
+brew bundle dump \
+  --brews --casks --taps \
+  --force --file="$root_path"/homebrew/Brewfile
+brew bundle dump \
+  --vscode \
+  --force --file="$root_path"/vscode/Brewfile
