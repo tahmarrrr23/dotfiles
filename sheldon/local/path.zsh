4970841a64a578fad700cb3c38909c2794b525ba
Fumiya Yamashita <4547751+tahmarrrr23@users.noreply.github.com>
2024-12-22 02:09:08 +0900

    update sheldon

--- sheldon/local/path.zsh
+++ sheldon/local/path.zsh
@@ -1,2 +1,4 @@
-[ -d "/opt/homebrew/opt/curl" ] && export PATH="/opt/homebrew/opt/curl/bin:$PATH"
-[ -d "/opt/homebrew/opt/gnu-tar" ] && export PATH="/opt/homebrew/opt/gnu-tar/libexec/gnubin:$PATH"
+[ -d "/opt/homebrew/opt/curl/bin" ] && export PATH="/opt/homebrew/opt/curl/bin:$PATH"
+[ -d "/opt/homebrew/opt/gnu-tar/libexec/gnubin" ] && export PATH="/opt/homebrew/opt/gnu-tar/libexec/gnubin:$PATH"
+[ -d "/opt/homebrew/opt/coreutils/libexec/gnubin" ] && export PATH="/opt/homebrew/opt/coreutils/libexec/gnubin:$PATH"
+[ -d "$HOME/.rd/bin" ] && export PATH="$HOME/.rd/bin:$PATH"
