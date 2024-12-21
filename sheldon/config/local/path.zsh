518f8d29cd7195926963d5f32485fffb8f6c63bf
Fumiya Yamashita <4547751+tahmarrrr23@users.noreply.github.com>
2024-12-22 01:10:55 +0900

    update sheldon

--- sheldon/config/local/path.zsh
+++ sheldon/config/local/path.zsh
@@ -1,7 +1,2 @@
-if [ -d "$HOMEBREW_PREFIX/Cellar/curl" ];then
-  export PATH="$HOMEBREW_PREFIX/opt/curl/bin:$PATH"
-fi
-
-if [ -d "$HOMEBREW_PREFIX/Cellar/gnu-tar" ];then
-  export PATH="$HOMEBREW_PREFIX/opt/gnu-tar/libexec/gnubin:$PATH"
-fi
+[ -d "/opt/homebrew/opt/curl" ] && export PATH="/opt/homebrew/opt/curl/bin:$PATH"
+[ -d "/opt/homebrew/opt/gnu-tar" ] && export PATH="/opt/homebrew/opt/gnu-tar/libexec/gnubin:$PATH"
--- sheldon/config/local/source.zsh
+++
@@ -1,7 +0,0 @@
-if [ -d "$HOMEBREW_PREFIX/Cellar/mise" ];then
-  eval "$(mise activate zsh)"
-fi
-
-if [ -d "$HOMEBREW_PREFIX/opt/starship" ];then
-  eval "$(starship init zsh)"
-fi
