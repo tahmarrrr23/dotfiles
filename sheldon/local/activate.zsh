ce119627e02d6461f4dcd12de4db88e32c865d2b
Fumiya Yamashita <4547751+tahmarrrr23@users.noreply.github.com>
2025-01-06 00:56:43 +0900

    change to docker completion from command

--- sheldon/local/activate.zsh
+++ sheldon/local/activate.zsh
@@ -1,3 +1,4 @@
 [ -f "/opt/homebrew/bin/mise" ] && eval "$(mise activate zsh)"
 [ -f "/opt/homebrew/bin/fzf" ] && eval "$(fzf --zsh)"
+[ -f "$HOME/.rd/bin/docker" ] && source <(docker completion zsh)
 [ -f "/opt/homebrew/bin/starship" ] && eval "$(starship init zsh)"
