e088e1464ae4596667e49ab4f5b4ae82e6b964a9
Fumiya Yamashita <138208610+tahmarrrr23@users.noreply.github.com>
2025-01-05 23:20:55 +0900

    add fzf

--- prompt/zsh/activate.zsh
+++ prompt/zsh/activate.zsh
@@ -1,2 +1,3 @@
-[ -f "/opt/homebrew/bin/mise" ] &&  eval "$(mise activate zsh)"
+[ -f "/opt/homebrew/bin/mise" ] && eval "$(mise activate zsh)"
+[ -f "/opt/homebrew/bin/fzf" ] && eval "$(fzf --zsh)"
 [ -f "/opt/homebrew/bin/starship" ] && eval "$(starship init zsh)"
