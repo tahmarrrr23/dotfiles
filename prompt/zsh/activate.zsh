ad6155ec382fe9b5bd9d2f05dae2e3979c04c76d
Fumiya Yamashita <4547751+tahmarrrr23@users.noreply.github.com>
2025-01-06 01:32:50 +0900

    add fzf keybind

--- prompt/zsh/activate.zsh
+++ prompt/zsh/activate.zsh
@@ -1,4 +1,16 @@
 command -v mise > /dev/null 2>&1 && eval "$(mise activate zsh)"
-command -v fzf > /dev/null 2>&1 && eval "$(fzf --zsh)"
+if command -v fzf > /dev/null 2>&1; then
+  eval "$(fzf --zsh)"
+  function ghq-fzf_change_directory() {
+    local src=$(ghq list | fzf --preview "eza -l -g -a --icons $(ghq root)/{} | tail -n+4 | awk '{print \$6\"/\"\$8\" \"\$9 \" \" \$10}'")
+    if [ -n "$src" ]; then
+      BUFFER="cd $(ghq root)/$src"
+      zle accept-line
+    fi
+    zle -R -c
+  }
+  zle -N ghq-fzf_change_directory
+  bindkey '^f' ghq-fzf_change_directory
+fi
 command -v docker > /dev/null 2>&1 && source <(docker completion zsh)
 command -v starship > /dev/null 2>&1 && eval "$(starship init zsh)"
