8a5a9e937f2771942ae2d66cdd3f52d217bea291
Fumiya Yamashita <4547751+tahmarrrr23@users.noreply.github.com>
2025-01-06 01:32:30 +0900

    add eza setting

--- sheldon/config/local/alias.zsh
+++ sheldon/config/local/alias.zsh
@@ -1,4 +1,11 @@
 alias relogin="exec $SHELL -l"
-alias ls="ls -1 --color=auto"
-alias ll="ls -l"
-alias la="ls -la"
+
+if command -v eza > /dev/null 2>&1; then
+  alias ls="eza -1"
+  alias ll="eza -l --icons"
+  alias la="eza -la --icons"
+else
+  alias ls="ls -1"
+  alias ll="ls -l"
+  alias la="ls -la"
+fi
