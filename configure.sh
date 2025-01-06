0961142bb7b9bb349eb0b4dd2ba7e79e0edd3891
Fumiya Yamashita <138208610+tahmarrrr23@users.noreply.github.com>
2025-01-06 22:51:03 +0900

    add ghostty👻

--- configure.sh
+++ configure.sh
@@ -17,10 +17,12 @@ brew bundle --file="$root_path/vscode/Brewfile"
 mkdir -p "$HOME"/.config/sheldon
 mkdir -p "$HOME"/.config/starship
 mkdir -p "$HOME"/.config/wezterm
+mkdir -p "$HOME"/.config/ghostty
 
 ln -s -f "$root_path/prompt/zsh/zshrc" "$HOME/.zshrc"
 ln -s -f "$root_path/prompt/zsh/zprofile" "$HOME/.zprofile"
 ln -s -f "$root_path/prompt/sheldon/plugins.toml" "$HOME/.config/sheldon/plugins.toml"
 ln -s -f "$root_path/prompt/starship/starship.toml" "$HOME/.config/starship.toml"
 ln -s -f "$root_path/wezterm/wezterm.lua" "$HOME/.config/wezterm/wezterm.lua"
+ln -s -f "$root_path/ghostty/config" "$HOME/.config/ghostty/config"
 ln -s -f "$root_path/vscode/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
