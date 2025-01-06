a67b5d4e9bb7806551c62e17d4e171e107160cc4
Fumiya Yamashita <138208610+tahmarrrr23@users.noreply.github.com>
2025-01-06 22:51:27 +0900

    update hist opt

--- prompt/zsh/history.zsh
+++ prompt/zsh/history.zsh
@@ -2,9 +2,10 @@ HISTFILE=~/.zsh_history
 HISTSIZE=1000000
 SAVEHIST=1000000
 HISTORY_IGNORE="(cd*|pwd|l[sal]*|code*|git*|relogin|history*|cat*|which*)"
-setopt SHARE_HISTORY
 setopt EXTENDED_HISTORY
-setopt HIST_REDUCE_BLANKS
-setopt HIST_IGNORE_SPACE
 setopt HIST_IGNORE_DUPS
+setopt HIST_IGNORE_SPACE
+setopt HIST_REDUCE_BLANKS
+setopt HIST_VERIFY
 setopt INC_APPEND_HISTORY
+setopt SHARE_HISTORY
