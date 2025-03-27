231da29280334f8eab1b46dea7e3ac1f52311eb6
Fumiya Yamashita <4547751+tahmarrrr23@users.noreply.github.com>
2025-03-27 18:04:53 +0900

    feat: remove deprecated path exports for gnu-tar and coreutils

--- sheldon/config/local/path.zsh
+++ sheldon/config/local/path.zsh
@@ -2,14 +2,6 @@ if [ -d "$HOMEBREW_PREFIX/Cellar/curl" ]; then
   export PATH="$HOMEBREW_REPOSITORY/opt/curl/bin:$PATH"
 fi
 
-if [ -d "$HOMEBREW_PREFIX/Cellar/gnu-tar" ]; then
-  export PATH="$HOMEBREW_REPOSITORY/opt/gnu-tar/libexec/gnubin:$PATH"
-fi
-
-if [ -d "$HOMEBREW_PREFIX/Cellar/coreutils" ]; then
-  export PATH="$HOMEBREW_REPOSITORY/opt/coreutils/libexec/gnubin:$PATH"
-fi
-
 if [ -d "$HOMEBREW_PREFIX/Caskroom/rancher" ]; then
   export PATH="$HOME/.rd/bin:$PATH"
 fi
