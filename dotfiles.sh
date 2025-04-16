19cceaf627690fe209d5ff19a1b8d46cb1f6aa8b
Fumiya Yamashita <4547751+tahmarrrr23@users.noreply.github.com>
2025-04-16 16:28:33 +0900

    feat: streamline dotfiles installation and configuration process

--- dotfiles.sh
+++ dotfiles.sh
@@ -2,44 +2,37 @@
 
 set -eu
 cd "$(dirname "$0")"
-root_path=$PWD
 
-brew_cmd="/opt/homebrew/bin/brew"
-if [ -f "$brew_cmd" ]; then
-  eval "$($brew_cmd shellenv)"
+if [ -f "/opt/homebrew/bin/brew" ]; then
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 else
   printf "Please install Homebrew\n"
   exit 1
 fi
 
+root_path=$PWD
+
 install() {
   brew bundle --file="$root_path/homebrew/Brewfile"
-
   zsh_bin=$(brew --prefix)/bin/zsh
   if ! fgrep -q $zsh_bin /etc/shells; then
     echo $zsh_bin | sudo tee -a /etc/shells
     chsh -s $zsh_bin
   fi
+  ln -fns "$root_path/zsh/zshrc" "$HOME/.zshrc"
+  ln -fns "$root_path/zsh/zprofile" "$HOME/.zprofile"
 }
 
 configure() {
   brew bundle --file="$root_path/vscode/Brewfile"
-
-  mkdir -p "$HOME/.config"
-
-  ln -fns "$root_path/zsh/zshrc" "$HOME/.zshrc"
-  ln -fns "$root_path/zsh/zprofile" "$HOME/.zprofile"
   ln -fns "$root_path/sheldon" "$HOME/.config/sheldon"
-  ln -fns "$root_path/starship/starship.toml" "$HOME/.config/starship.toml"
   ln -fns "$root_path/ghostty" "$HOME/.config/ghostty"
   ln -fns "$root_path/vscode/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
 }
 
 backup() {
-  brew bundle dump --brews --casks --taps \
-    --force --file="$root_path"/homebrew/Brewfile
-  brew bundle dump --vscode \
-    --force --file="$root_path"/vscode/Brewfile
+  brew bundle dump -f --file="$root_path"/homebrew/Brewfile --no-vscode 
+  brew bundle dump -f --file="$root_path"/vscode/Brewfile --vscode
 }
 
 case "$1" in
@@ -54,6 +47,5 @@ case "$1" in
     ;;
   *)
     echo "Usage: $0 {install|configure|backup}"
-    exit 1
     ;;
 esac
