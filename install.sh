ca7cba7c14a16146ddae02f063d6cf2212b68d20
Fumiya Yamashita <4547751+tahmarrrr23@users.noreply.github.com>
2025-04-25 09:10:30 +0900

    feat(ghostty): update ghostty themes and font

--- install.sh
+++ install.sh
@@ -30,6 +30,7 @@ ln -nfs "$PWD/wezterm/wezterm.lua" "$HOME/.config/wezterm/wezterm.lua"
 # ghostty
 mkdir -p "$HOME"/.config/ghostty
 ln -nfs "$PWD/ghostty/config" "$HOME/.config/ghostty/config"
+ln -nfs "$PWD/ghostty/themes" "$HOME/.config/ghostty/themes"
 
 # linearmouse
 mkdir -p "$HOME"/.config/linearmouse
