71d8c866506e3fd4405ba3e1e23c980af6f67fcc
Fumiya Yamashita <4547751+tahmarrrr23@users.noreply.github.com>
2025-08-23 23:56:42 +0900

    feat(sheldon): update docker aliases for MySQL and Redoc with timezone and port adjustments

--- sheldon/config/local/alias.zsh
+++ sheldon/config/local/alias.zsh
@@ -1,5 +1,5 @@
 alias relogin="exec $SHELL -l"
 alias reset-launchpad="sudo find 2>/dev/null /private/var/folders/ -type d -name com.apple.dock.launchpad -exec rm -rf {} +; killall Dock"
 
-alias docker-mysql="docker run -d -it --name mysql -p 3306:3306 -v mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=root mysql:latest"
-alias docker-redoc="docker run -d -it --name redoc -p 8081:80 -e SPEC_URL=http://localhost:8080/openapi.json redocly/redoc"
+alias docker-mysql="docker run -d -it --name mysql -p 3306:3306 -v mysql:/var/lib/mysql -e TZ=Asia/Tokyo -e MYSQL_ROOT_PASSWORD=root mysql"
+alias docker-redoc="docker run -d -it --name redoc -p 8079:80 -e SPEC_URL=http://localhost:8080/openapi.json redocly/redoc"
