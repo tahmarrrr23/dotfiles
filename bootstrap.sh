e2ed2613659217a6401cc2f2c7a206bb7580e293
Fumiya Yamashita <4547751+tahmarrrr23@users.noreply.github.com>
2025-10-06 19:26:52 +0900

    feat: enhance bootstrap script with logging and step tracking

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,12 +2,31 @@
 
 set -u
 
+step_counter=0
+
+log() {
+  printf '%s\n' "$*"
+}
+
+log_indent() {
+  log "-> $*"
+}
+
+log_step() {
+  step_counter=$((step_counter + 1))
+  log "Step ${step_counter}: $*"
+}
+
+log_step "Install Homebrew"
 if [ ! -f "/opt/homebrew/bin/brew" ]; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+  log_indent "Already installed"
 fi
-
 echo "eval \"\$(/opt/homebrew/bin/brew shellenv)\"" > "$HOME/.zprofile"
+log_indent "Environment in .zprofile"
 
+log_step "Remove '.localized' files"
 localized_dirs=(
   "$HOME/Applications"
   "$HOME/Documents"
@@ -20,9 +39,9 @@ localized_dirs=(
   "$HOME/Library"
   "/Applications"
 )
-
 for dir in "${localized_dirs[@]}"; do
   localized_file="$dir/.localized"
+  log_indent "Removing $localized_file"
   if [ -e "$localized_file" ]; then
     rm "$localized_file"
   fi
