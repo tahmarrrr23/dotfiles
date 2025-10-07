f1c1fbb3bee2f7c86d67d66a3ac45a094b0afa05
Fumiya Yamashita <4547751+tahmarrrr23@users.noreply.github.com>
2025-10-08 07:43:54 +0900

    feat(sheldon): refine ds_store aliases for improved functionality

--- sheldon/config/local/fd.zsh
+++ sheldon/config/local/fd.zsh
@@ -2,5 +2,5 @@ if ! command -v fd >/dev/null 2>&1; then
   return
 fi
 
-alias ds_store="fd -HI -t f '.DS_Store'"
-alias ds_store-remove="fd -HI -t f '.DS_Store' -x rm -iv"
+alias ds_store="fd -H '^\.DS_Store$' -tf $HOME"
+alias ds_store-remove="fd -H '^\.DS_Store$' -tf $HOME -x rm -v"
