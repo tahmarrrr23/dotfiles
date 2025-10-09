414a29073b40a7f34eae86aff930c2ebd042b839
Fumiya Yamashita <4547751+tahmarrrr23@users.noreply.github.com>
2025-10-09 21:22:35 +0900

    feat(sheldon): add ghq by fzf

---
+++ sheldon/config/local/fzf.zsh
@@ -0,0 +1,12 @@
+if command -v ghq > /dev/null 2>&1 ; then
+  function ghq-fzf() {
+    local target_dir=$(ghq list -p | fzf --query="$LBUFFER")
+    if [ -n "$target_dir" ]; then
+      BUFFER="cd ${target_dir}"
+      zle accept-line
+    fi
+    zle reset-prompt
+  }
+  zle -N ghq-fzf
+  bindkey "^g" ghq-fzf
+fi
