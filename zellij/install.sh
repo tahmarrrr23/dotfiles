f0742b4213873bc275cb6954e6085cb68585d819
Fumiya Yamashita <4547751+tahmarrrr23@users.noreply.github.com>
2026-01-11 22:55:57 +0900

    feat: remove zellij

--- zellij/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-root_directory="$(cd "$(dirname "$0")/.." && pwd)"
-
-echo "-> [zellij] configure"
-
-rm -rf "$HOME/.config/zellij"
-ln -sfn "$root_directory/zellij/config" "$HOME/.config/zellij"
