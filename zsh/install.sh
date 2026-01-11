af5e59281173c90bbbbeb58c470e365802b9679b
Fumiya Yamashita <4547751+tahmarrrr23@users.noreply.github.com>
2026-01-11 21:24:45 +0900

    refactor(zsh): reorder zshrc settings and improve install script output

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+set -eu
+
 root_directory="$(cd "$(dirname "$0")/.." && pwd)"
 
-echo "-> [zsh] configure"
+echo "[zsh] configure"
 
 rm -rf "$HOME/.zshrc"
 ln -sfn "$root_directory/zsh/config/zshrc" "$HOME/.zshrc"
