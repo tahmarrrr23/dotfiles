cc1be9a352bb7888b4186f5ac1f168b159efd730
Fumiya Yamashita <4547751+tahmarrrr23@users.noreply.github.com>
2026-01-11 22:55:31 +0900

    refactor(rio): update window decorations and cursor settings for improved usability

--- rio/install.sh
+++ rio/install.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+set -eu
+
 root_directory="$(cd "$(dirname "$0")/.." && pwd)"
 
-echo "-> [rio] configure"
+echo "[rio] configure"
 
 rm -rf "$HOME/.config/rio"
 ln -sfn "$root_directory/rio/config" "$HOME/.config/rio"
