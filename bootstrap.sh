693f6e2c15b518541196fd6bdbc5fcc77f7b0d69
Fumiya Yamashita <4547751+tahmarrrr23@users.noreply.github.com>
2026-01-11 21:02:06 +0900

    refactor(bootstrap): simplify script by removing logging functions and improving echo statements

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,32 +1,18 @@
 #!/bin/bash
+set -eu
 
-set -u
+echo "[bootstrap] install homebrew"
 
-step_counter=0
-
-log() {
-  printf '%s\n' "$*"
-}
-
-log_indent() {
-  log "-> $*"
-}
-
-log_step() {
-  step_counter=$((step_counter + 1))
-  log "Step ${step_counter}: $*"
-}
-
-log_step "Install Homebrew"
 if [ ! -f "/opt/homebrew/bin/brew" ]; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
-  log_indent "Already installed"
+  echo "-> already installed"
 fi
-echo "eval \"\$(/opt/homebrew/bin/brew shellenv)\"" > "$HOME/.zprofile"
-log_indent "Environment in .zprofile"
 
-log_step "Remove '.localized' files"
+echo "eval \"\$(/opt/homebrew/bin/brew shellenv)\"" > "$HOME/.zprofile"
+
+echo "[bootstrap] remove .localized files"
+
 localized_dirs=(
   "$HOME/Applications"
   "$HOME/Documents"
@@ -39,10 +25,11 @@ localized_dirs=(
   "$HOME/Library"
   "/Applications"
 )
+
 for dir in "${localized_dirs[@]}"; do
   localized_file="$dir/.localized"
-  log_indent "Removing $localized_file"
+  echo "-> removing $localized_file"
   if [ -e "$localized_file" ]; then
-    rm "$localized_file"
+    rm -rf "$localized_file"
   fi
 done
