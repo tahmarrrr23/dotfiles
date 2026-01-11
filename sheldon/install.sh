20e95f24bbbd86006a5c23afd0d47160861d4636
Fumiya Yamashita <4547751+tahmarrrr23@users.noreply.github.com>
2026-01-11 21:50:59 +0900

    refactor(sheldon): remove unused fzf plugin and improve install script output

--- sheldon/install.sh
+++ sheldon/install.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+set -eu
+
 root_directory="$(cd "$(dirname "$0")/.." && pwd)"
 
-echo "-> [sheldon] configure"
+echo "[sheldon] configure"
 
 rm -rf "$HOME/.config/sheldon"
 ln -sfn "$root_directory/sheldon/config" "$HOME/.config/sheldon"
