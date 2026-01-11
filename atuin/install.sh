d786fb36a22a6fdbe85bf8f5da72e7c33070c77d
Fumiya Yamashita <4547751+tahmarrrr23@users.noreply.github.com>
2026-01-11 21:44:59 +0900

    refactor(atuin): update config and install script for improved clarity and error handling

--- atuin/install.sh
+++ atuin/install.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+set -eu
+
 root_directory="$(cd "$(dirname "$0")/.." && pwd)"
 
-echo "-> [atuin] configure"
+echo "[atuin] configure"
 
 rm -rf "$HOME/.config/atuin"
 ln -sfn "$root_directory/atuin/config" "$HOME/.config/atuin"
