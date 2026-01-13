452dc1c41abe58a126b463028ce178c12e9499d2
Fumiya Yamashita <4547751+tahmarrrr23@users.noreply.github.com>
2026-01-14 07:54:31 +0900

    feat(sheldon): add sorting option

--- sheldon/local/eza.zsh
+++ sheldon/local/eza.zsh
@@ -3,5 +3,5 @@ if ! command -v eza >/dev/null 2>&1; then
 fi
 
 alias ls="eza --group-directories-first --icons"
-alias ll="eza -lgb --time-style long-iso --group-directories-first --icons"
-alias la="eza -lgba --time-style long-iso --group-directories-first --icons"
+alias ll="eza -lgb --time-style long-iso --group-directories-first --icons --sort=Name"
+alias la="eza -lgba --time-style long-iso --group-directories-first --icons --sort=Name"
