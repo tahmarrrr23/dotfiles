5fe4e0c1ebf7bac7c438d7bc510380069f24dead
Fumiya Yamashita <4547751+tahmarrrr23@users.noreply.github.com>
2026-01-31 17:55:28 +0900

    feat: remove hammerspoon

--- hammerspoon/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -eu
-
-root_directory="$(cd "$(dirname "$0")/.." && pwd)"
-
-echo "[hammerspoon] configure"
-
-rm -rf "$HOME/.hammerspoon/init.lua"
-ln -sfn "$root_directory/hammerspoon/config/init.lua" "$HOME/.hammerspoon/init.lua"
