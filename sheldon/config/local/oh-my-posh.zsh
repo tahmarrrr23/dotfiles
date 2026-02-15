704320ec2dcedc55256b7aa177fbbd35ed86f5c9
Fumiya Yamashita <4547751+tahmarrrr23@users.noreply.github.com>
2026-02-16 03:40:53 +0900

    feat(sheldon): update oh-my-posh theme

--- sheldon/config/local/oh-my-posh.zsh
+++ sheldon/config/local/oh-my-posh.zsh
@@ -1,3 +1,4 @@
 [[ ! -x ${commands[oh-my-posh]} ]] && return
 
-eval "$(oh-my-posh init zsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/amro.omp.json')"
+theme="json"
+eval "$(oh-my-posh init zsh --config "https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/$theme.omp.json")"
