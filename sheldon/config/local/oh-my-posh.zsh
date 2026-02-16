a93c12f4e462e973b8927b04ccc31b127a1e4496
Fumiya Yamashita <4547751+tahmarrrr23@users.noreply.github.com>
2026-02-17 06:36:37 +0900

    feat(oh-my-posh): change theme

--- sheldon/config/local/oh-my-posh.zsh
+++ sheldon/config/local/oh-my-posh.zsh
@@ -1,4 +1,4 @@
 [[ ! -x ${commands[oh-my-posh]} ]] && return
 
-theme="json"
+theme="avit"
 eval "$(oh-my-posh init zsh --config "https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/$theme.omp.json")"
